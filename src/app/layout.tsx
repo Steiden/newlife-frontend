efd25d4972c0c0a83cffe931eac67efd6578b34f
Daniil <baldindaniilza@mail.ru>
2024-06-18 09:19:17 +0500

    База

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 
 const montserrat = Montserrat({ subsets: ["latin", "cyrillic"] });
 
 export const metadata: Metadata = {
-  title: "New Life",
+  title: "Новая жизнь",
   description: "Приложение по поиску пропавших животных",
 };
 
