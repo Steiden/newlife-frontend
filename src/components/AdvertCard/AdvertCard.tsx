bd8113946a9f24c7fb6fdd16f2ff098ec1c798f9
Daniil <baldindaniilza@mail.ru>
2024-06-18 13:33:03 +0500

    Вывод карточек объявлений

--- src/components/AdvertCard/AdvertCard.tsx
+++ src/components/AdvertCard/AdvertCard.tsx
@@ -7,18 +7,26 @@ type PropsType = {
 };
 
 export const AdvertCard = (props: PropsType) => {
+	const getImage = (source: string): string => {
+		console.log(`/img/animals/${source}`);
+
+		return `/img/animals/${source}`;
+	};
+
 	return (
 		<article className={styles.card}>
 			<Image
-				src={props.advert?.advert_photos ? props.advert.advert_photos[0].image : ""}
+				src={getImage(
+					props.advert?.advert_photos?.length ? props.advert.advert_photos[0].image : ""
+				)}
 				alt={props.advert.title}
 				width={200}
 				height={250}
-                className={styles.card__image}
+				className={styles.card__image}
 			/>
 			<div className={styles.card__content}>
 				<h4 className={styles.card__title}>{props.advert.title}</h4>
-				<p className={styles.card__text}>&bull;{props.advert?.animal_type?.name}</p>
+				<p className={styles.card__text}>&bull; {props.advert?.animal_type?.name}</p>
 				<p className={styles.card__text}>{props.advert.description}</p>
 			</div>
 		</article>
